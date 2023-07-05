d213f3e4c04833437eb16276e3a342449b73af7c
Светлана Югай <135803951+SvetlanaYgay@users.noreply.github.com>
2023-07-05 15:39:42 +0600

    Add files via upload

---
+++ 29/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = 8;
+int[] array = GetBinArray(num);
+Console.Write($"[{String.Join(", ", array)}]");
+Console.Write($"[");
+for (int i = 0; i < num - 1; i++)
+{
+    Console.Write($"{array[i]}, ");
+}Console.Write($"{array[num - 1]}]");
+
+
+int[] GetBinArray(int size){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++){
+        result[i] = new Random().Next(10);
+    }
+    return result;
+}
