6fa36270dd844b5d5410db7a851358b034bb2deb
Светлана Югай <135803951+SvetlanaYgay@users.noreply.github.com>
2023-07-05 15:33:56 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,26 @@
-﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-//3, 5 -> 243 (3⁵)
-//2, 4 -> 16
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
 
-int Exponentiation(int A, int B)
-{
-  int result = 1;
-  for(int i=1; i <= B; i++)
+Console.WriteLine($"Выдаёт сумму цифр в числе");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN)
   {
-    result = result * A;
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
   }
-    // int result = Math.Pow(A, B);
-    return result;
-}
 
-  Console.Write("Введите число A: ");
-  int numberA = Convert.ToInt32(Console.ReadLine());
-  Console.Write("Введите число B: ");
-  int numberB = Convert.ToInt32(Console.ReadLine());
-
-  int exponentiation = Exponentiation(A, B);
-  Console.WriteLine("Ответ: " + exponentiation);
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
